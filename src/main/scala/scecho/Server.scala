085499c15d8def6ab6be3b8887cf44080d7177e1
aguestuser <guest.austin@gmail.com>
2015-01-29 14:57:54 -0500

    Flesh out Main, add #accept, #read, #write, #writeOnce to Server

--- src/main/scala/scecho/Server.scala
+++ src/main/scala/scecho/Server.scala
@@ -3,10 +3,8 @@ package scecho
 import java.net.InetSocketAddress
 import java.nio.ByteBuffer
 import java.nio.channels.{AsynchronousSocketChannel, CompletionHandler, AsynchronousServerSocketChannel}
-
+import scala.concurrent.{ExecutionContext, Promise, Future}
 import scala.util.{Failure, Success, Try}
-
-
 
 /**
  * Created by aguestuser on 1/28/15.
@@ -14,38 +12,92 @@ import scala.util.{Failure, Success, Try}
 
 object Main extends App {
   Try(args(0).toInt) match {
-    case Success(i) => Server(i)
-    case Failure(e) => throw new Exception("Argument to scecho must be a valid Integer")
+
+    case Failure(e) => throw new IllegalArgumentException("Argument to scecho must be a valid Integer")
+    case Success(i) =>
+
+      val server = Server(i)
+
+      val result = for {
+        chn <- server.accept(server.chn)
+        input <- server.read(chn)
+        output <- server.write(input, chn)
+      } yield output
+
+      result onSuccess { case res => println("Finished echoing!") }
+      result onFailure { case err => throw err }
   }
 }
 
 case class Server(port: Int) {
-  val server = AsynchronousServerSocketChannel.open().bind(new InetSocketAddress(port))
-  var buf = ByteBuffer.allocate(1024) // <- is this where i should declare this?
 
-  server.accept(null, new CompletionHandler {
-    def completed(chn: AsynchronousSocketChannel, /* what goes here? What is a `_ >:NotInferredA` ???*/) = {
+  val chn = AsynchronousServerSocketChannel.open().bind(new InetSocketAddress(port))
 
-      // why does intellij tell me i've passed too many arguments to server.accept?
+  def accept(chn: AsynchronousServerSocketChannel): Future[AsynchronousSocketChannel] = {
+    val p = Promise[AsynchronousSocketChannel]()
+    chn.accept(null, new CompletionHandler[AsynchronousSocketChannel, Void] {
+      def completed(client: AsynchronousSocketChannel, att: Void) =  p success { client }
+      def failed(e: Throwable, att: Void) = p failure { e }
+    })
+    p.future
+  }
 
-      // should i be doing some intermediate step on the channel here
-      // instead of going ahead and writing to it?
+  def read(chn: AsynchronousSocketChannel): Future[Array[Byte]] = {
+    val buf = ByteBuffer.allocate(1024)
+    val p = Promise[Array[Byte]]()
+    chn.read(buf, null, new CompletionHandler[Integer, Void] {
+      def completed(numRead: Integer, att: Void) = {
+        println("Read %d bytes".formatted(numRead.toString))
+        buf.flip()
+        p success { buf.array() }
+      }
+      def failed(e: Throwable, att: Void) = p failure { e }
+    })
+    p.future
 
-      //this is what i do with a buffer... but i'm not sure i should be doing this here??
-      buf.clear()
-      chn.read(buf)
-      buf.flip()
-      chn.write(buf)
-
-      // UMMM.... this all feels very side-effecty! What do I return from here?
+  }
+  def write(bs: Array[Byte], chn: AsynchronousSocketChannel)(implicit executor: ExecutionContext): Future[Unit] = {
+    for {
+      numWritten <- writeOnce(bs, chn)
+    } yield {
+      if (numWritten == bs.length) Future.successful(())
+      else write(bs.drop(numWritten), chn)
     }
-  })
-  // why does IntelliJ tell me: the following about server.accept:
-  // `Type mismatch:
-  // expected:
-  //    CompletionHandler[AsynchronousSocketChannel,_ >:NotInferedA],
-  // actual:
-  //    CompletionHandler with Object {def completed(chn: AsynchronousSocketChannel): Future[Integer]
+  }
 
-
+  def writeOnce(bs: Array[Byte], chn: AsynchronousSocketChannel): Future[Integer] = {
+    val p = Promise[Integer]()
+    chn.write(ByteBuffer.wrap(bs), null, new CompletionHandler[Integer, Void] {
+      def completed(numWritten: Integer, att: Void) = {
+        println("Echoed %d bytes".formatted(numWritten.toString))
+        p success { numWritten }
+      }
+      def failed(e: Throwable, att: Void) = p failure { e }
+    })
+    p.future
+  }
 }
+
+
+//i **THINK** the for comprehension in the main loop
+//does more or less the below. is that right?
+//
+//val cnxn = server.accept(server.chn)
+//
+//cnxn onSuccess {
+//  case ch =>
+//    val input = server.read(ch)
+//
+//    input onSuccess {
+//      case bs =>
+//        val output = server.write(bs,ch)
+//
+//    }
+//
+//    input onFailure {
+//      case e => throw e
+//    }
+//}
+//cnxn onFailure {
+//  case e => throw e
+//}
