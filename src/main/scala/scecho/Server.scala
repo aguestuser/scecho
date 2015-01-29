a6d526465f2c2cf9a5d41b281397bdce8a064dbd
aguestuser <guest.austin@gmail.com>
2015-01-28 19:52:02 -0500

    Add more confused comments

--- src/main/scala/scecho/Server.scala
+++ src/main/scala/scecho/Server.scala
@@ -21,10 +21,31 @@ object Main extends App {
 
 case class Server(port: Int) {
   val server = AsynchronousServerSocketChannel.open().bind(new InetSocketAddress(port))
+  val buf = ByteBuffer.allocate(1024) // <- is this where i should declare this?
+
   server.accept(null, new CompletionHandler {
-    def completed(buf: ByteBuffer, /* what goes here?*/) = {
+    def completed(chn: AsynchronousSocketChannel, /* what goes here? What is a `_ >:NotInferredA` ???*/) = {
+
+      // why does intellij tell me i've passed too many arguments to server.accept?
+
+      // should i be doing some intermediate step on the channel here
+      // instead of going ahead and writing to it?
+
+      //this is what i do with a buffer... but i'm not sure i should be doing this here??
       buf.clear()
-      /// WHAT GOES HERE?
+      chn.read(buf)
+      buf.flip()
+      chn.write(buf)
+
+      // UMMM.... this all feels very side-effecty! What do I return from here?
     }
   })
+  // why does IntelliJ tell me: the following about server.accept:
+  // `Type mismatch:
+  // expected:
+  //    CompletionHandler[AsynchronousSocketChannel,_ >:NotInferedA],
+  // actual:
+  //    CompletionHandler with Object {def completed(chn: AsynchronousSocketChannel): Future[Integer]
+
+
 }
